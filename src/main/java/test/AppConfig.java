0b9d573c94aef44e549246a0c0ab396acfd51d52
krsdm <krsdm@ya.ru>
2021-09-29 05:33:14 +0300

    bugfix

--- src/main/java/test/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("test")
-public class AppConfig {
-}
